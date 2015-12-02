e075b34cea353a140ed8a73c865f5ea58a4611dd
Isaac Bowen <irbowen@umich.edu>
2015-12-02 17:34:24 -0500

    Day1 p2

--- day1.cpp
+++ day1.cpp
@@ -12,5 +12,18 @@ int main() {
   }
   auto floor_num = count(begin(v), end(v), '(') - count(begin(v), end(v), ')');
   cout << "Value: " << floor_num << endl;
+  auto sum = 0;
+  for (size_t i = 0; i < v.size(); i++) {
+    if (v.at(i) == '(') {
+      sum++;
+    }
+    else {
+      sum--;
+    }
+    if (sum < 0) {
+      cout << "Index is " << i << endl;
+      break;
+    }
+  }
   return 0;
 }
