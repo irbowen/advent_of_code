762f9ee18009c0b3dae8780be2b79001273dee1a
Isaac Bowen <irbowen@umich.edu>
2015-12-13 14:29:06 -0500

    Day4 part 2

--- day4.py
+++ day4.py
@@ -2,12 +2,20 @@ import md5
 
 key = 'bgvyzdsv'
 count = 0
+five_zeros = ' '
+six_zeros = ' '
+five_flag = False
+six_flag = False
 while (True):
   m = md5.new(key + str(count)).hexdigest()
   if (m[0] == '0' and m[1] == '0' and m[2] == '0' and m[3] == '0' and m[4] == '0'):
-    print m
+    five_zeros = m
+    five_flag = True
+    print 'Four zeros: ' + str(count)
+  if (m[0] == '0' and m[1] == '0' and m[2] == '0' and m[3] == '0' and m[4] == '0' and m[5] == '0'):
+    six_zeroes = m
+    six_flag = True
+    print 'Six zeros: ' + str(count)
+  if (five_flag and six_flag):
     break
-  count = count + 1
-print m
-print count
-  
+  count = count + 1 
